3948aa925e4fafdc83bcbd3098e9e77eb7924b8d
BboySlowly <5505002757@rumail.ru.ac.th>
2021-01-04 05:10:44 +0700

    API for Department Screen : POST, PUT and DELETE.

--- WebApplication1/Controllers/DepartmentController.cs
+++ WebApplication1/Controllers/DepartmentController.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -26,6 +27,72 @@ namespace WebApplication1.Controllers
             }
 
             return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        public string Post(Department dep)
+        {
+            try
+            {
+                string query = @"INSERT INTO `employeedb`.`dbo.department` (`DepartmentName`) VALUES('" + dep.DepartmentName + @"')";
+                DataTable table = new DataTable();
+                using (var con = new MySqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new MySqlCommand(query, con))
+                using (var da = new MySqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Added Successfully!!";
+            }
+            catch
+            {
+                return "Failed to Add!!";
+            }
+        }
+
+        public string Put(Department dep)
+        {
+            try
+            {
+                string query = @"UPDATE `employeedb`.`dbo.department` SET `DepartmentName` = '" + dep.DepartmentName + "' WHERE(`DepartmentId` = '" + dep.DepartmentId + @"')";
+                DataTable table = new DataTable();
+                using (var con = new MySqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new MySqlCommand(query, con))
+                using (var da = new MySqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Updated Successfully!!";
+            }
+            catch
+            {
+                return "Failed to Update!!";
+            }
+        }
+
+        public string Delete(int id)
+        {
+            try
+            {
+                string query = @"DELETE FROM employeedb.`dbo.department` WHERE DepartmentId=" + id + @"";
+                DataTable table = new DataTable();
+                using (var con = new MySqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new MySqlCommand(query, con))
+                using (var da = new MySqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Deleted Successfully!!";
+            }
+            catch
+            {
+                return "Failed to Add!!";
+            }
         }
     }
 }
