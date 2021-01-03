2d00a72da63e61cd64e5881e57a0188a74de9248
BboySlowly <5505002757@rumail.ru.ac.th>
2021-01-04 06:28:33 +0700

    APIs for Employee Screen.

--- WebApplication1/Controllers/EmployeeController.cs
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -11,11 +11,16 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
-    public class DepartmentController : ApiController
+    public class EmployeeController : ApiController
     {
         public HttpResponseMessage Get()
         {
-            string query = @"SELECT DepartmentId,DepartmentName FROM employeedb.`dbo.department`";
+            string query = @"SELECT EmployeeId,
+                                    EmployeeName,
+                                    Department,
+                                    DateOfjoining,
+                                    PhotoFileName
+                                    FROM employeedb.`dbo.employee`";
             DataTable table = new DataTable();
             using (var con = new MySqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
             using (var cmd = new MySqlCommand(query, con))
@@ -28,11 +33,16 @@ namespace WebApplication1.Controllers
             return Request.CreateResponse(HttpStatusCode.OK, table);
         }
 
-        public string Post(Department dep)
+        public string Post(Employee emp)
         {
             try
             {
-                string query = @"INSERT INTO `employeedb`.`dbo.department` (`DepartmentName`) VALUES('" + dep.DepartmentName + @"')";
+                string query = @"INSERT INTO `employeedb`.`dbo.employee`
+                                    (`EmployeeName`,`Department`,`DateOfJoining`,`PhotoFileName`)
+                                    VALUES('" + emp.EmployeeName + @"',
+                                           '" + emp.Department + @"',
+                                           '" + emp.DateOfJoining + @"',
+                                           '" + emp.PhotoFileName + @"')";
                 DataTable table = new DataTable();
                 using (var con = new MySqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
                 using (var cmd = new MySqlCommand(query, con))
@@ -50,11 +60,16 @@ namespace WebApplication1.Controllers
             }
         }
 
-        public string Put(Department dep)
+        public string Put(Employee emp)
         {
             try
             {
-                string query = @"UPDATE `employeedb`.`dbo.department` SET `DepartmentName` = '" + dep.DepartmentName + "' WHERE(`DepartmentId` = '" + dep.DepartmentId + @"')";
+                string query = @"UPDATE `employeedb`.`dbo.employee` SET
+                                        `EmployeeName` = '" + emp.EmployeeName + @"',
+                                        `Department` = '" + emp.Department + @"',
+                                        `DateOfJoining` = '" + emp.DateOfJoining + @"',
+                                        `PhotoFileName` = '" + emp.PhotoFileName + @"'
+                                        WHERE(`EmployeeId` = '" + emp.EmployeeId + @"')";
                 DataTable table = new DataTable();
                 using (var con = new MySqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
                 using (var cmd = new MySqlCommand(query, con))
@@ -76,7 +91,7 @@ namespace WebApplication1.Controllers
         {
             try
             {
-                string query = @"DELETE FROM employeedb.`dbo.department` WHERE DepartmentId=" + id + @"";
+                string query = @"DELETE FROM employeedb.`dbo.employee` WHERE EmployeeId=" + id + @"";
                 DataTable table = new DataTable();
                 using (var con = new MySqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
                 using (var cmd = new MySqlCommand(query, con))
@@ -92,6 +107,23 @@ namespace WebApplication1.Controllers
             {
                 return "Failed to Add!!";
             }
+        }
+
+        [Route("api/Employee/GetAllDepartmentNames")]
+        [HttpGet]
+        public HttpResponseMessage GetAllDepartmentNames()
+        {
+            string query = @"SELECT DepartmentName FROM employeedb.`dbo.department`";
+            DataTable table = new DataTable();
+            using (var con = new MySqlConnection(ConfigurationManager.ConnectionStrings["EmployeeAppDB"].ConnectionString))
+            using (var cmd = new MySqlCommand(query, con))
+            using (var da = new MySqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
         }
     }
 }
--- WebApplication1/Models/Employee.cs
+++ WebApplication1/Models/Employee.cs
@@ -8,9 +8,9 @@ namespace WebApplication1.Models
     public class Employee
     {
         public int EmployeeId { get; set; }
-        public string Employeename { get; set; }
+        public string EmployeeName { get; set; }
         public string Department { get; set; }
-        public string DateofJoining { get; set; }
+        public string DateOfJoining { get; set; }
         public string PhotoFileName { get; set; }
     }
 }
